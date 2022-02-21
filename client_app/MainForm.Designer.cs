d8c528bcc25fcb1e9fcf8560ee0945070684793a
aibek <a.kerimbay@gmail.com>
2022-02-21 16:52:15 +0600

    client app

---
+++ client_app/MainForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace SimpleCryptoClient_v1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOpenPort = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnClosePort = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClearOutput = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnOpenPort
+            // 
+            this.btnOpenPort.Location = new System.Drawing.Point(128, 12);
+            this.btnOpenPort.Name = "btnOpenPort";
+            this.btnOpenPort.Size = new System.Drawing.Size(106, 51);
+            this.btnOpenPort.TabIndex = 0;
+            this.btnOpenPort.Text = "Open COM";
+            this.btnOpenPort.UseVisualStyleBackColor = true;
+            this.btnOpenPort.Click += new System.EventHandler(this.btnOpenPort_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 141);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(436, 297);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnClosePort
+            // 
+            this.btnClosePort.Enabled = false;
+            this.btnClosePort.Location = new System.Drawing.Point(240, 12);
+            this.btnClosePort.Name = "btnClosePort";
+            this.btnClosePort.Size = new System.Drawing.Size(103, 51);
+            this.btnClosePort.TabIndex = 3;
+            this.btnClosePort.Text = "Close COM";
+            this.btnClosePort.UseVisualStyleBackColor = true;
+            this.btnClosePort.Click += new System.EventHandler(this.btnClosePort_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.ReadBufferSize = 128;
+            this.serialPort1.ReadTimeout = 500;
+            this.serialPort1.WriteBufferSize = 128;
+            this.serialPort1.WriteTimeout = 500;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(349, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(99, 51);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClearOutput
+            // 
+            this.btnClearOutput.Enabled = false;
+            this.btnClearOutput.Location = new System.Drawing.Point(297, 69);
+            this.btnClearOutput.Name = "btnClearOutput";
+            this.btnClearOutput.Size = new System.Drawing.Size(151, 52);
+            this.btnClearOutput.TabIndex = 9;
+            this.btnClearOutput.Text = "Clear Output";
+            this.btnClearOutput.UseVisualStyleBackColor = true;
+            this.btnClearOutput.Click += new System.EventHandler(this.btnClearOutput_Click);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Enabled = false;
+            this.btnTest.Location = new System.Drawing.Point(128, 69);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(163, 52);
+            this.btnTest.TabIndex = 13;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(110, 21);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "300",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "74880",
+            "115200",
+            "230400",
+            "250000",
+            "500000",
+            "1000000",
+            "2000000"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 42);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(110, 21);
+            this.comboBox2.TabIndex = 15;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Encrypt",
+            "Decrypt"});
+            this.comboBox3.Location = new System.Drawing.Point(12, 86);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(110, 21);
+            this.comboBox3.TabIndex = 16;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 450);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnClearOutput);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClosePort);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnOpenPort);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SimpleCryptoClient_v3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenPort;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnClosePort;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClearOutput;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+    }
+}
+
---
+++ client_app/MainForm.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using static System.IO.Ports.SerialPort;
+
+namespace SimpleCryptoClient_v1
+{
+    public partial class MainForm : Form
+    {
+        Stopwatch stopwatch;
+        double frequency;
+        bool highRes;
+
+        public string input;
+        public string output;
+        public string label;
+        public MainForm()
+        {
+            InitializeComponent();
+            frequency = Stopwatch.Frequency;
+            highRes = Stopwatch.IsHighResolution;
+            stopwatch = new Stopwatch();
+        }
+
+        private void btnOpenPort_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBox1.Text;
+            serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
+            serialPort1.Open();
+            Thread.Sleep(100);
+            if (serialPort1.IsOpen)
+            {
+                textBox1.AppendText("Port open" + Environment.NewLine);
+                btnOpenPort.Enabled    = false;
+                btnExit.Enabled        = false;
+                btnClosePort.Enabled   = true;
+                btnClearOutput.Enabled = true;
+                btnTest.Enabled        = true;
+            }
+                
+        }
+
+        private void btnClosePort_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            Thread.Sleep(100);
+            if (!serialPort1.IsOpen)
+            {
+                textBox1.AppendText("Port closed" + Environment.NewLine);
+                btnOpenPort.Enabled    = true;
+                btnExit.Enabled        = true;
+                btnClosePort.Enabled   = false;
+                btnClearOutput.Enabled = false;
+                btnTest.Enabled        = false;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClearOutput_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText(Environment.NewLine);
+            textBox1.AppendText(label + Environment.NewLine);
+            textBox1.AppendText("Stopwatch" + Environment.NewLine);
+            textBox1.AppendText("Frequency=" + frequency.ToString() + Environment.NewLine);
+            textBox1.AppendText("IsHighResolution=" + highRes.ToString() + Environment.NewLine);
+            count = 0; right = 0; wrong = 0;
+            encrypt();
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string line = serialPort1.ReadLine();
+           
+            this.BeginInvoke(new LineReceivedEvent(LineReceived), line);
+        }
+
+        public int count = 0; int right = 0; int wrong = 0;
+
+        private delegate void LineReceivedEvent(string line);
+        private void LineReceived(string line)
+        {
+            runStopwatch = false;
+
+            if (line.Equals(output))
+                right++;
+            else
+                wrong++;
+
+            count++;
+
+            if (count <= 1000)
+                encrypt();
+            else
+                textBox1.AppendText($"Count= {count} Right={right} Wrong= {wrong}" + Environment.NewLine);
+        }
+
+        //-----------------------------------------------------------------
+
+        public void encrypt()
+        {
+            runStopwatch = true;
+            Write(input);
+        }
+
+        public bool runStopwatch
+        {
+            get { 
+                return stopwatch.IsRunning; 
+            }
+            set {
+                if (value)
+                {
+                    if (!stopwatch.IsRunning)
+                        stopwatch.Start();
+                }
+                else
+                {
+                    if (stopwatch.IsRunning)
+                    {
+                        stopwatch.Stop();
+                        textBox1.AppendText(stopwatch.ElapsedTicks.ToString() + Environment.NewLine);
+                        stopwatch.Reset();
+                    }
+                }
+            }
+        }
+
+        public void Write(string data)
+        {
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    byte[] OutString = new byte[64];
+                    OutString = Encoding.ASCII.GetBytes(data + Environment.NewLine);
+                    serialPort1.Write(OutString, 0, OutString.Length);
+                }
+                catch
+                {
+                    textBox1.AppendText("Error writing to serial" + Environment.NewLine);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = GetPortNames();
+            foreach (string port in ports)
+                comboBox1.Items.Add(port);
+
+            if (comboBox1.Items.Count > 0)
+                comboBox1.SelectedIndex = 0;
+                
+            comboBox2.SelectedIndex = 9;
+            comboBox3.SelectedIndex = 0;
+
+            if (comboBox3.SelectedIndex == 0)
+            {
+                input = "$10 00112233445566778899AABBCCDDEEFF";
+                output = "45C8188BB1DF1A812734C3DF5E2C9FAF";
+                label = "Encrypt Test";
+            }
+            else
+            {
+                input = "$20 45C8188BB1DF1A812734C3DF5E2C9FAF";
+                output = "00112233445566778899AABBCCDDEEFF";
+                label = "Decrypt Test";
+            }
+        }
+
+
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox3.SelectedIndex == 0)
+            {
+                input = "$10 00112233445566778899AABBCCDDEEFF";
+                output = "45C8188BB1DF1A812734C3DF5E2C9FAF";
+                label = "Encrypt Test";
+            }
+            else
+            {
+                input = "$20 45C8188BB1DF1A812734C3DF5E2C9FAF";
+                output = "00112233445566778899AABBCCDDEEFF";
+                label = "Decrypt Test";
+            }
+        }
+    }
+}
